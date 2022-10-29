c5e3e1f1490f5abbade581bb7d25bfdd50ff6f42
Ken Sparby <sparby.k@gmail.com>
2022-10-29 13:52:24 +0200

    Finished(-ish)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./style.scss";
+
+const emailInput = document.querySelector("#emailInput");
+const subscribeForm = document.querySelector("#subscribeForm");
+
+const validateEmail = function (target) {
+  const regex =
+    /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+  return regex.test(target);
+};
+
+subscribeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const invalidEmail = document.querySelector(".invalidEmail");
+  if (validateEmail(emailInput.value)) {
+    invalidEmail.style.display = "none";
+  } else {
+    invalidEmail.style.display = "block";
+  }
+});
